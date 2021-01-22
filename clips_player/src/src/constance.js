492b867591521e8b8076e2d56f7f114359f90af5
Son Dieu <StigerDieu@gmail.com>
2021-01-22 22:34:13 +0700

    rebuild

--- clips_player/src/src/constance.js
+++ clips_player/src/src/constance.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const baseUrl = "/clips_player"
 
-export const VIEW_HOME = baseUrl + '/'
+export const VIEW_HOME = baseUrl
 export const VIEW_README = baseUrl + '/read-me'
 export const VIEW_PRIVACY = baseUrl + '/privacy'
 
