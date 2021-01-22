c943163f7a0ee431d0a473e67a6c34caaf6fd2c9
Son Dieu <StigerDieu@gmail.com>
2021-01-22 22:55:06 +0700

    remove unused slash

--- clips_player/src/src/constance.js
+++ clips_player/src/src/constance.js
@@ -1,9 +1,7 @@
 import React from "react";
 
-export const baseUrl = "/"
-
-export const VIEW_HOME = baseUrl
-export const VIEW_README = baseUrl + '/read-me'
-export const VIEW_PRIVACY = baseUrl + '/privacy'
+export const VIEW_HOME = "/"
+export const VIEW_README = '/read-me'
+export const VIEW_PRIVACY = '/privacy'
 
 export const AppContext = React.createContext({}) 
